b0eec57ecdeab05f939905df3632b2ec17ec6235
Sahana Shetty <sahana.shetty998@gmail.com>
2024-11-05 09:39:50 +0530

    added square movement of drone

---
+++ python/reconaissance/drone_control/drone_square_movement.py
@@ -0,0 +1,16 @@
+from djitellopy import Tello
+
+tello = Tello()
+
+tello.connect()
+tello.takeoff()
+#tello.rotate_counter_clockwise(90)
+tello.move_forward(50)
+tello.rotate_clockwise(90)
+tello.move_forward(50)
+tello.rotate_clockwise(90)
+tello.move_forward(50)
+tello.rotate_clockwise(90)
+tello.move_forward(50)
+tello.rotate_clockwise(90)
+tello.land()
