ffe6082fa003eaba17e695156338b23c96fc0b8e
Sahana Shetty <sahana.shetty998@gmail.com>
2024-11-06 10:41:51 +0530

    improved code of square movement of drone

--- python/reconaissance/drone_control/drone_square_movement.py
+++ python/reconaissance/drone_control/drone_square_movement.py
@@ -4,13 +4,8 @@ tello = Tello()
 
 tello.connect()
 tello.takeoff()
-#tello.rotate_counter_clockwise(90)
-tello.move_forward(50)
-tello.rotate_clockwise(90)
-tello.move_forward(50)
-tello.rotate_clockwise(90)
-tello.move_forward(50)
-tello.rotate_clockwise(90)
-tello.move_forward(50)
-tello.rotate_clockwise(90)
+
+for _ in range(4):
+    tello.move_forward(50)
+    tello.rotate_counter_clockwise(90)
 tello.land()
